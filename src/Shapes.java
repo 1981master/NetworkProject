55eae8728171a6cf827402709ebe997cacd2778e
etae100 <114241528+etae100@users.noreply.github.com>
2022-10-16 20:39:17 -0400

    first commit

---
+++ src/Shapes.java
@@ -0,0 +1,120 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+public class Shapes {
+	
+	private static JButton button;
+	private static ShapesPanel panel;
+	
+	Shapes(){
+		
+	}
+	
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setSize(800,400);
+		frame.setLocationRelativeTo(null);
+	    panel = new ShapesPanel();
+		frame.add(panel);
+		
+
+		JLabel label = new JLabel("Emission rate", SwingConstants.LEFT);
+		panel.add(label);
+		//JComboBox
+        String[] emissionRate = {"100 packet/s", "300 packet/s", "500 packet/s"};
+        JComboBox<String> jComboBox = new JComboBox<>(emissionRate);
+        jComboBox.setBounds(80, 50, 140, 20);
+        panel.add(jComboBox);
+
+	    
+	    
+	    
+		JLabel label2 = new JLabel("Transmission rate", SwingConstants.LEFT);
+		panel.add(label2);
+		//JComboBox
+        String[] transmissionRate = {"300 packet/s", "500 packet/s", "800 packet/s"};
+        JComboBox<String> jComboBox2 = new JComboBox<>(transmissionRate);
+        jComboBox2.setBounds(80, 50, 140, 20);
+        panel.add(jComboBox2);
+		
+		
+		//button
+	    button = new JButton("Start");
+	    panel.add(button);
+		
+		//button
+	    JButton button2 = new JButton("Reset");
+	    panel.add(button2);
+	    
+	    
+		JLabel label3 = new JLabel("msec", SwingConstants.LEFT);
+		label3.setBounds(0, 700, 90, 20);
+		panel.add(label3);
+	    
+		
+		button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String selected =  jComboBox.getItemAt(jComboBox.getSelectedIndex());
+                label3.setText("it's work...");
+                
+                
+                //runProg();
+                
+            }
+        });
+
+
+		//JPanel panel2 = new JPanel();
+		//frame.add(panel2);
+	      
+//		
+//	    JPanel panel2 = new JPanel();
+//	      frame.getContentPane();
+
+//	      Dimension size = button.getPreferredSize();
+//	      button.setBounds(300, 180, size.width, size.height);
+//	      panel2.setLayout(null);
+//	      panel2.add(button);
+//	      panel2.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		//DrawCable p = new DrawCable();
+
+		
+		frame.setVisible(true);
+		
+		while (true) {
+			panel.moveRec();
+			panel.repaint();
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
+		
+	}
+//	protected static void runProg() {
+//		while (true) {
+//			panel.moveRec();
+//			panel.repaint();
+//			try {
+//				Thread.sleep(10);
+//			} catch (InterruptedException e) {
+//				e.printStackTrace();
+//			}
+//		}
+//	}
+
+}
---
+++ src/ShapesPanel.java
@@ -0,0 +1,159 @@
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+
+import javax.swing.JPanel;
+
+public class ShapesPanel extends JPanel{
+	
+		private int x = 0;
+		private int y = 0;
+		private int color =100;
+
+		public void moveRec() {
+			x = x + 1;
+			y = y + 1;
+		}
+		
+		protected void movers(Graphics g, int cx, int cy, int cz) {
+			Graphics2D g2d = (Graphics2D) g;
+			g2d.setStroke(new BasicStroke(5));
+
+			if(x>=40 && x<=280) {
+				g2d.drawRect(40+x,150,25,20);
+				g2d.setColor(Color.ORANGE);
+				g2d.fillRect(40+x, 150, 25, 20);
+
+				
+				g2d.setColor(Color.GREEN);
+				g2d.fillRect(x+10, 150, 25, 20);
+				g2d.drawRect(x,150,25,20);
+				
+				g2d.setColor(Color.MAGENTA);
+				g2d.fillRect(-40+x, 150, 25, 20);
+				g2d.drawRect(x,150,25,20);
+				
+				g2d.setColor(Color.YELLOW);
+				g2d.fillRect(-80+x, 150, 25, 20);
+				g2d.drawRect(x,150,25,20);
+				
+				
+				
+				g2d.setColor(Color.RED);
+				g2d.fillRect(-120+x, 150, 25, 20);
+				g2d.drawRect(x,150,25,20);
+				
+				g2d.setColor(Color.PINK);
+				g2d.fillRect(-160+x, 150, 25, 20);
+				g2d.drawRect(x,150,25,20);
+				
+				g2d.setColor(Color.CYAN);
+				g2d.fillRect(-200+x, 150, 25, 20);
+				g2d.drawRect(x,150,25,20);
+				
+				g2d.setColor(Color.BLUE);
+				g2d.fillRect(-240+x, 150, 25, 20);
+				g2d.drawRect(x,150,25,20);
+			}
+			
+		
+
+		 if(x>=310 && x<=410) {
+				g2d.setColor(new Color(0, 0, 0, 128));
+				g2d.fillRect(40+x, 120, 10, 80);
+
+			} if(x>=410 && x<=420) {
+				//g2d.drawRect(460, 135, 30, 50);
+				g2d.fillRect(460, 135, 30, 50);
+			}
+			//rec moving throw the end of the cable.
+			 if(x>=420 && x<=630) {
+				//g2d.drawRect(490,150,25,20);
+				g2d.fillRect(80+x,150,25,20);
+			}
+			 
+
+			
+			
+			
+			
+			
+	
+		}
+		
+		
+		protected void draw(Graphics g, int cx, int cy, int cz) {
+			Graphics2D g2d = (Graphics2D) g;
+			g2d.setStroke(new BasicStroke(5));
+			
+			//start cable
+			g2d.setColor(Color.BLACK);
+			g2d.drawRect(40,150,310,20);
+			
+			//queue
+			g2d.setColor(Color.BLACK);
+			int loop =0;
+			while(loop <=100) {
+				g2d.drawRect(350+loop, 120, 10, 80);
+				loop += 10;
+
+			}
+
+			
+			
+			//out of queue
+			g2d.drawRect(460, 135, 30, 50);
+			
+			//end cable
+			g2d.drawRect(490,150,250,20);
+			
+			//draw the rect moving at the end of the cable.
+			g2d.setColor(Color.BLACK);
+			Rectangle2D.Double cableWidth = new Rectangle2D.Double(350,150,50,20);
+			
+			movers(g,cx,cy,cz);
+			//movers(g,cx,cy,cz);
+	
+
+			
+		}
+		
+		
+		
+		@Override
+		protected void paintComponent(Graphics g){
+			super.paintComponent(g);
+			draw(g,0,0,0);
+			
+			
+		
+			
+
+		}
+		
+
+}
+
+
+//for(int i =0; i< 10;i++) {
+//for(int j =0; j< 10;j++) {
+//	g2d.drawRect(350+loop, 120, 10, 80);
+//	loop += 10;
+//}
+//loop+=0;
+//}
+
+//g2d.drawRect(350, 120, 10, 80);
+//g2d.drawRect(360, 120, 10, 80);
+//g2d.drawRect(370, 120, 10, 80);
+//g2d.drawRect(380, 120, 10, 80);
+//g2d.drawRect(390, 120, 10, 80);
+//g2d.drawRect(400, 120, 10, 80);
+//g2d.drawRect(410, 120, 10, 80);
+//g2d.drawRect(420, 120, 10, 80);
+//g2d.drawRect(430, 120, 10, 80);
+//g2d.drawRect(440, 120, 10, 80);
+//g2d.drawRect(450, 120, 10, 80);
